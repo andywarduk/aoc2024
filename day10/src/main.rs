3e387fd56205694e7ccff1a76af025e6921bf45d
Andy Ward <andy.ward.uk@gmail.com>
2024-12-10 07:52:20 +0000

    day 10 tidy

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -14,49 +14,49 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn part1(input: &[InputEnt]) -> u64 {
-    let heads = input
-        .iter()
-        .enumerate()
-        .fold(Vec::new(), |mut acc, (y, l)| {
-            acc.extend(l.iter().enumerate().filter_map(
-                |(x, h)| {
-                    if *h == 0 { Some((x, y)) } else { None }
-                },
-            ));
-            acc
-        });
-
-    heads
+    heads(input)
         .into_iter()
         .map(|(x, y)| {
             let mut dests = HashSet::new();
+
             walk1(input, x, y, 1, &mut dests);
+
             dests.len() as u64
         })
         .sum()
 }
 
 fn walk1(input: &[InputEnt], x: usize, y: usize, h: u8, dests: &mut HashSet<(usize, usize)>) {
-    [[0, -1isize], [1, 0], [0, 1], [-1, 0]]
-        .iter()
-        .for_each(|[dx, dy]| {
-            if (*dx >= 0 || x > 0) && (*dy >= 0 || y > 0) {
-                let nx = (x as isize + dx) as usize;
-                let ny = (y as isize + dy) as usize;
-
-                if nx < input[0].len() && ny < input.len() && input[ny][nx] == h {
-                    if h == 9 {
-                        dests.insert((nx, ny));
-                    } else {
-                        walk1(input, nx, ny, h + 1, dests);
-                    }
-                }
-            }
-        })
+    pos_from(input, x, y, h).for_each(|(nx, ny)| {
+        if h == 9 {
+            dests.insert((nx, ny));
+        } else {
+            walk1(input, nx, ny, h + 1, dests);
+        }
+    })
 }
 
 fn part2(input: &[InputEnt]) -> u64 {
-    let heads = input
+    heads(input)
+        .into_iter()
+        .map(|(x, y)| walk2(input, x, y, 1))
+        .sum()
+}
+
+fn walk2(input: &[InputEnt], x: usize, y: usize, h: u8) -> u64 {
+    pos_from(input, x, y, h)
+        .map(|(nx, ny)| {
+            if h == 9 {
+                1
+            } else {
+                walk2(input, nx, ny, h + 1)
+            }
+        })
+        .sum()
+}
+
+fn heads(input: &[InputEnt]) -> Vec<(usize, usize)> {
+    input
         .iter()
         .enumerate()
         .fold(Vec::new(), |mut acc, (y, l)| {
@@ -66,31 +66,24 @@ fn part2(input: &[InputEnt]) -> u64 {
                 },
             ));
             acc
-        });
-
-    heads.into_iter().map(|(x, y)| walk2(input, x, y, 1)).sum()
+        })
 }
 
-fn walk2(input: &[InputEnt], x: usize, y: usize, h: u8) -> u64 {
-    [[0, -1isize], [1, 0], [0, 1], [-1, 0]]
-        .iter()
-        .map(|[dx, dy]| {
-            if (*dx >= 0 || x > 0) && (*dy >= 0 || y > 0) {
-                let nx = (x as isize + dx) as usize;
-                let ny = (y as isize + dy) as usize;
+const DIRS: [[isize; 2]; 4] = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
-                if nx < input[0].len() && ny < input.len() && input[ny][nx] == h {
-                    return if h == 9 {
-                        1
-                    } else {
-                        walk2(input, nx, ny, h + 1)
-                    };
-                }
+fn pos_from(input: &[InputEnt], x: usize, y: usize, h: u8) -> impl Iterator<Item = (usize, usize)> {
+    DIRS.into_iter().filter_map(move |[dx, dy]| {
+        if (dx >= 0 || x > 0) && (dy >= 0 || y > 0) {
+            let nx = (x as isize + dx) as usize;
+            let ny = (y as isize + dy) as usize;
+
+            if nx < input[0].len() && ny < input.len() && input[ny][nx] == h {
+                return Some((nx, ny));
             }
+        }
 
-            0
-        })
-        .sum()
+        None
+    })
 }
 
 // Input parsing
