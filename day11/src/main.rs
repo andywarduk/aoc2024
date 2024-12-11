7701e0b598aa185bc3524c0c8027b01ab733438a
Andy Ward <andy.ward.uk@gmail.com>
2024-12-11 09:44:13 +0000

    day 11

---
+++ day11/src/main.rs
@@ -0,0 +1,145 @@
+use std::{
+    collections::{HashMap, VecDeque},
+    error::Error,
+};
+
+use aoc::input::parse_input_line;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Get input
+    let input = parse_input_line(11, input_transform)?;
+
+    // Run parts
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+
+    Ok(())
+}
+
+fn part1(input: &InputEnt) -> u64 {
+    count(input, 25)
+}
+
+fn part2(input: &InputEnt) -> u64 {
+    count(input, 75)
+}
+
+fn count(input: &InputEnt, iters: u8) -> u64 {
+    let mut work = VecDeque::new();
+    let mut note = HashMap::new();
+
+    // Build initial work queue
+    (0..input.len()).for_each(|i| {
+        work.push_back((input[i], iters));
+    });
+
+    // Process work queue
+    while let Some((num, iters)) = work.pop_front() {
+        let mut rework = true;
+
+        if num == 0 {
+            // 0 -> 1
+            if iters == 1 {
+                note.insert((num, iters), 1u64);
+                rework = false;
+            } else if let Some(count) = note.get(&(1, iters - 1)) {
+                note.insert((num, iters), *count);
+                rework = false;
+            } else {
+                work.push_front((1, iters - 1));
+            }
+        } else {
+            let log10 = num.ilog10();
+
+            if (log10 & 1) == 1 {
+                // Split even number
+                if iters == 1 {
+                    note.insert((num, iters), 2);
+                    rework = false;
+                } else {
+                    let div = 10u64.pow((log10 + 1) / 2);
+
+                    let num1 = num / div;
+                    let num2 = num % div;
+
+                    match (note.get(&(num1, iters - 1)), note.get(&(num2, iters - 1))) {
+                        (Some(count1), Some(count2)) => {
+                            note.insert((num, iters), count1 + count2);
+                            rework = false;
+                        }
+                        (Some(_), None) => {
+                            work.push_front((num2, iters - 1));
+                        }
+                        (None, Some(_)) => {
+                            work.push_front((num1, iters - 1));
+                        }
+                        (None, None) => {
+                            work.push_front((num2, iters - 1));
+                            work.push_front((num1, iters - 1));
+                        }
+                    }
+                }
+            } else {
+                // Odd number - multiply by 2024
+                if iters == 1 {
+                    note.insert((num, iters), 1u64);
+                    rework = false;
+                } else {
+                    let new = num * 2024;
+
+                    if let Some(count) = note.get(&(new, iters - 1)) {
+                        note.insert((num, iters), *count);
+                        rework = false;
+                    } else {
+                        work.push_front((new, iters - 1));
+                    }
+                }
+            }
+        }
+
+        if rework {
+            work.push_back((num, iters));
+        }
+    }
+
+    input
+        .iter()
+        .map(|num| *note.get(&(*num, iters)).unwrap())
+        .sum()
+}
+
+// Input parsing
+
+type InputEnt = Vec<u64>;
+
+fn input_transform(line: String) -> InputEnt {
+    line.split_ascii_whitespace()
+        .map(|ns| ns.parse::<u64>().expect("not an integer"))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE1: &str = "0 1 10 99 999";
+    const EXAMPLE2: &str = "125 17";
+
+    #[test]
+    fn test1() {
+        let input = input_transform(EXAMPLE1.to_string());
+        assert_eq!(count(&input, 1), 7);
+    }
+
+    #[test]
+    fn test2() {
+        let input = input_transform(EXAMPLE2.to_string());
+        assert_eq!(count(&input, 6), 22);
+    }
+
+    #[test]
+    fn test3() {
+        let input = input_transform(EXAMPLE2.to_string());
+        assert_eq!(part1(&input), 55312);
+    }
+}
