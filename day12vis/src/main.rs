2627ad6fee0958e60b44c58ab77d2b8d46369bb4
Andy Ward <andy.ward.uk@gmail.com>
2024-12-13 11:20:17 +0000

    day 12 vis

--- day12vis/src/main.rs
+++ day12vis/src/main.rs
@@ -49,12 +49,11 @@ fn get_shapes(input: &[InputEnt]) -> Vec<Shape> {
 const CELLSIZE: usize = 6;
 
 fn draw(input: &[InputEnt], shapes: &[Shape]) -> Result<(), Box<dyn Error>> {
-    let chars = input
-        .iter()
-        .flat_map(|l| l.iter().copied())
-        .collect::<BTreeSet<char>>();
+    let chars: BTreeSet<char> = input.iter().flat_map(|l| l.iter().copied()).collect();
 
     let mut palette = vec![[0, 0, 0], [255, 255, 255]];
+
+    let pal_start = palette.len() as u8;
 
     for i in 0..chars.len() {
         let deg = (360.0 / chars.len() as f64) * i as f64;
@@ -62,6 +61,22 @@ fn draw(input: &[InputEnt], shapes: &[Shape]) -> Result<(), Box<dyn Error>> {
         let hsl = HSL {
             h: deg,
             s: 1.0,
+            l: 0.5,
+        };
+
+        let (r, g, b) = hsl.to_rgb();
+
+        palette.push([r, g, b]);
+    }
+
+    let pal_dim = palette.len() as u8;
+
+    for i in 0..chars.len() {
+        let deg = (360.0 / chars.len() as f64) * i as f64;
+
+        let hsl = HSL {
+            h: deg,
+            s: 0.1,
             l: 0.5,
         };
 
@@ -79,19 +94,46 @@ fn draw(input: &[InputEnt], shapes: &[Shape]) -> Result<(), Box<dyn Error>> {
         1,
     )?;
 
-    let mut frame = gif.empty_frame();
+    draw_frame(&mut gif, shapes, |c, fence| {
+        if fence {
+            0
+        } else {
+            chars.iter().position(|&cp| cp == c).unwrap() as u8 + pal_start
+        }
+    })?;
 
-    for shape in shapes {
-        let colour = chars.iter().position(|&c| c == shape.c).unwrap() as u8 + 2;
-        draw_shape(&mut frame, shape, colour);
-    }
+    draw_frame(&mut gif, shapes, |c, fence| {
+        let pos = chars.iter().position(|&cp| cp == c).unwrap() as u8;
 
-    gif.draw_frame(frame, 0)?;
+        if fence {
+            pos + pal_start
+        } else {
+            pos + pal_dim
+        }
+    })?;
 
     Ok(())
 }
 
-fn draw_shape(frame: &mut [Vec<u8>], shape: &Shape, colour: u8) {
+fn draw_frame<F>(gif: &mut Gif, shapes: &[Shape], colour: F) -> Result<(), Box<dyn Error>>
+where
+    F: Fn(char, bool) -> u8,
+{
+    let mut frame = gif.empty_frame();
+
+    for shape in shapes {
+        draw_shape(&mut frame, shape, &colour);
+    }
+
+    gif.draw_frame(frame, 300)?;
+
+    Ok(())
+}
+
+fn draw_shape<F>(frame: &mut [Vec<u8>], shape: &Shape, colour: F)
+where
+    F: Fn(char, bool) -> u8,
+{
     let drawhoriz = |frame: &mut [Vec<u8>], y: usize, x1: usize, x2: usize, col: u8| {
         for x in x1..=x2 {
             frame[y][x] = col;
@@ -104,7 +146,7 @@ fn draw_shape(frame: &mut [Vec<u8>], shape: &Shape, colour: u8) {
         let mut sy = 1;
         let mut ey = CELLSIZE - 2;
 
-        if x == 0 || shape.squares.contains(&(x - 1, y)) {
+        if x > 0 && shape.squares.contains(&(x - 1, y)) {
             sx = 0;
         }
 
@@ -112,7 +154,7 @@ fn draw_shape(frame: &mut [Vec<u8>], shape: &Shape, colour: u8) {
             ex = CELLSIZE - 1;
         }
 
-        if y == 0 || shape.squares.contains(&(x, y - 1)) {
+        if y > 0 && shape.squares.contains(&(x, y - 1)) {
             sy = 0;
         }
 
@@ -123,8 +165,22 @@ fn draw_shape(frame: &mut [Vec<u8>], shape: &Shape, colour: u8) {
         let gx = x * CELLSIZE;
         let gy = y * CELLSIZE;
 
+        let fence_colour = colour(shape.c, true);
+        let inner_colour = colour(shape.c, false);
+
+        for i in 0..sy {
+            drawhoriz(frame, gy + i, gx, gx + CELLSIZE - 1, fence_colour);
+        }
+
         for i in sy..=ey {
-            drawhoriz(frame, gy + i, gx + sx, gx + ex, colour);
+            drawhoriz(frame, gy + i, gx + sx, gx + ex, inner_colour);
+
+            drawhoriz(frame, gy + i, gx, gx + sx - 1, fence_colour);
+            drawhoriz(frame, gy + i, gx + ex + 1, gx + CELLSIZE - 1, fence_colour);
+        }
+
+        for i in (ey + 1)..CELLSIZE {
+            drawhoriz(frame, gy + i, gx, gx + CELLSIZE - 1, fence_colour);
         }
     }
 }
