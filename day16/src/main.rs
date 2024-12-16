d823f0de9febb980292a165a5a78f07858ddd1f2
Andy Ward <andy.ward.uk@gmail.com>
2024-12-16 08:31:01 +0000

    day 15

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     error::Error,
 };
 
@@ -8,15 +8,121 @@ use aoc::input::parse_input_vec;
 fn main() -> Result<(), Box<dyn Error>> {
     // Get input
     let input = parse_input_vec(16, input_transform)?;
+    let graph = build_graph(&input);
+    let (best_score, best_routes) = walk(&graph);
 
     // Run parts
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    println!("Part 1: {}", best_score);
+    println!("Part 2: {}", part2(&graph, &best_routes));
 
     Ok(())
 }
 
-fn part1(input: &[InputEnt]) -> u64 {
+fn part2(graph: &Graph, best_routes: &[Vec<usize>]) -> u64 {
+    let coords = best_routes
+        .iter()
+        .flat_map(|r| r.iter().flat_map(|e| graph.edges[*e].path.iter().copied()))
+        .collect::<HashSet<Coord>>();
+
+    coords.len() as u64
+}
+
+fn walk(graph: &Graph) -> (u64, Vec<Vec<usize>>) {
+    let mut best_score = u64::MAX;
+    let mut best_routes = Vec::new();
+
+    let mut scores = HashMap::new();
+
+    let mut workq = VecDeque::new();
+
+    workq.push_back(Work {
+        node: graph.start,
+        dir: Dir::E,
+        score: 0,
+        route: Vec::new(),
+    });
+
+    while let Some(work) = workq.pop_front() {
+        if work.node == graph.end {
+            if work.score < best_score {
+                best_score = work.score;
+                best_routes = vec![work.route];
+            } else if work.score == best_score {
+                best_routes.push(work.route);
+            }
+            continue;
+        }
+
+        if let Some(score) = scores.get_mut(&(work.node, work.dir)) {
+            if *score < work.score {
+                continue;
+            }
+            *score = work.score;
+        } else {
+            scores.insert((work.node, work.dir), work.score);
+        }
+
+        for en in graph.nodes[work.node].edges.iter() {
+            let edge = &graph.edges[*en];
+
+            if work.dir.opposite() == edge.indir {
+                continue;
+            }
+
+            let mut score = work.score + edge.score;
+
+            if work.dir != edge.indir {
+                score += 1000;
+            }
+
+            if score > best_score {
+                continue;
+            }
+
+            let mut new_route = work.route.clone();
+
+            new_route.push(*en);
+
+            workq.push_back(Work {
+                node: edge.tonode,
+                dir: edge.outdir,
+                score,
+                route: new_route,
+            });
+        }
+    }
+
+    (best_score, best_routes)
+}
+
+struct Work {
+    node: usize,
+    dir: Dir,
+    score: u64,
+    route: Vec<usize>,
+}
+
+struct Node {
+    pos: Coord,
+    edges: Vec<usize>,
+}
+
+struct Edge {
+    tonode: usize,
+    indir: Dir,
+    outdir: Dir,
+    score: u64,
+    path: Vec<Coord>,
+}
+
+struct Graph {
+    start: usize,
+    end: usize,
+    nodes: Vec<Node>,
+    edges: Vec<Edge>,
+}
+
+fn build_graph(input: &[InputEnt]) -> Graph {
     // Find start
     let spos = input
         .iter()
@@ -47,87 +153,87 @@ fn part1(input: &[InputEnt]) -> u64 {
         })
         .unwrap();
 
-    let mut workq = VecDeque::new();
+    // Find nodes
+    let mut nodes = Vec::new();
 
-    workq.push_back(Work1 {
-        pos: spos,
-        dir: Dir::E,
-        score: 0,
-        route: Vec::new(),
-    });
-
-    let mut best_score = u64::MAX;
-
-    let mut scores = HashMap::new();
-
-    while let Some(work) = workq.pop_front() {
-        if work.pos == epos {
-            println!("{}: {:?}", work.score, work.route);
-            if work.score < best_score {
-                best_score = work.score;
-            }
-            continue;
-        }
-
-        let new_dirs = dirs(input, work.pos);
-
-        let mut add_route = false;
-
-        if new_dirs.len() > 2 {
-            // > 2 choice of direction
-            if let Some(score) = scores.get(&(work.pos, work.dir)) {
-                if *score < work.score {
-                    continue;
-                }
-            } else {
-                scores.insert((work.pos, work.dir), work.score);
-            }
-
-            add_route = true;
-        }
-
-        for (new_dir, new_pos) in new_dirs {
-            if work.dir.opposite() == new_dir {
+    for (y, l) in input.iter().enumerate() {
+        for (x, t) in l.iter().enumerate() {
+            if *t == MapTile::Wall {
                 continue;
             }
 
-            let mut score = work.score + 1;
+            let pos = (x, y);
+            let dirs = dirs(input, pos, None);
 
-            if work.dir != new_dir {
-                score += 1000;
+            if dirs.len() > 2 || pos == spos || pos == epos {
+                nodes.push(Node {
+                    pos,
+                    edges: Vec::new(),
+                });
             }
-
-            if score > best_score {
-                continue;
-            }
-
-            let mut new_route = work.route.clone();
-
-            if add_route {
-                new_route.push((work.pos, new_dir));
-            }
-
-            workq.push_back(Work1 {
-                pos: new_pos,
-                dir: new_dir,
-                score,
-                route: new_route,
-            })
         }
     }
 
-    best_score
-}
+    let node_map = nodes
+        .iter()
+        .enumerate()
+        .map(|(i, n)| (n.pos, i))
+        .collect::<HashMap<_, _>>();
 
-struct Work1 {
-    pos: Coord,
-    dir: Dir,
-    score: u64,
-    route: Vec<(Coord, Dir)>,
-}
+    // Build edges
+    let mut edges = Vec::new();
 
-fn part2(input: &[InputEnt]) -> u64 {
-    0 // TODO
+    for n in nodes.iter_mut() {
+        for (dir, mut next) in dirs(input, n.pos, None) {
+            let mut cdir = dir;
+            let mut path = Vec::new();
+            let mut score = 1;
+
+            path.push(n.pos);
+
+            loop {
+                path.push(next);
+
+                let dirs = dirs(input, next, Some(cdir.opposite()));
+
+                if dirs.is_empty() {
+                    break;
+                }
+
+                if let Some(n2) = node_map.get(&next) {
+                    n.edges.push(edges.len());
+
+                    edges.push(Edge {
+                        tonode: *n2,
+                        indir: dir,
+                        outdir: cdir,
+                        score,
+                        path,
+                    });
+
+                    break;
+                }
+
+                if dirs[0].0 != cdir {
+                    score += 1000;
+                    cdir = dirs[0].0;
+                }
+
+                next = dirs[0].1;
+                score += 1;
+            }
+        }
+    }
+
+    let start = *node_map.get(&spos).unwrap();
+    let end = *node_map.get(&epos).unwrap();
+
+    Graph {
+        start,
+        end,
+        nodes,
+        edges,
+    }
 }
 
 type Coord = (usize, usize);
@@ -158,13 +264,21 @@ const DIRS: [(Dir, [isize; 2]); 4] = [
     (Dir::W, [-1, 0]),
 ];
 
-fn dirs(input: &[InputEnt], c: Coord) -> Vec<(Dir, Coord)> {
+fn dirs(input: &[InputEnt], c: Coord, skip_dir: Option<Dir>) -> Vec<(Dir, Coord)> {
     DIRS.iter()
         .filter_map(move |&(mdir, [dx, dy])| match c.0.checked_add_signed(dx) {
             Some(nx) if nx < input[0].len() => match c.1.checked_add_signed(dy) {
                 Some(ny) if ny < input.len() => {
                     if input[ny][nx] != MapTile::Wall {
-                        Some((mdir, (nx, ny)))
+                        if let Some(skip_dir) = skip_dir {
+                            if mdir == skip_dir {
+                                None
+                            } else {
+                                Some((mdir, (nx, ny)))
+                            }
+                        } else {
+                            Some((mdir, (nx, ny)))
+                        }
                     } else {
                         None
                     }
@@ -247,12 +361,20 @@ mod tests {
     #[test]
     fn test1() {
         let input = parse_test_vec(EXAMPLE1, input_transform).unwrap();
-        assert_eq!(part1(&input), 7036);
+        let graph = build_graph(&input);
+        let (best_score, best_routes) = walk(&graph);
+
+        assert_eq!(best_score, 7036);
+        assert_eq!(part2(&graph, &best_routes), 45);
     }
 
     #[test]
     fn test2() {
         let input = parse_test_vec(EXAMPLE2, input_transform).unwrap();
-        assert_eq!(part1(&input), 11048);
+        let graph = build_graph(&input);
+        let (best_score, best_routes) = walk(&graph);
+
+        assert_eq!(best_score, 11048);
+        assert_eq!(part2(&graph, &best_routes), 64);
     }
 }
