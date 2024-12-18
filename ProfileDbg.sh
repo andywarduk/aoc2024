8908d862cca0461f7b39031d1e5940a0ba30e1b1
Andy Ward <andy.ward.uk@gmail.com>
2024-12-18 17:49:32 +0000

    Profiling

---
+++ ProfileDbg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "x$1" == "x" ]; then
+	echo "Must give a day number"
+	exit 1
+fi
+
+if [ "$1" -lt 1 -o "$1" -gt 25 ]; then
+	echo "Day number invalid"
+	exit 1
+fi
+
+daypad="$(printf %02d $1)"
+
+mkdir -p flamegraphs >/dev/null 2>&1
+
+file=flamegraphs/day$daypad-dbg.svg
+
+cargo flamegraph --root --dev --bin day$daypad --output "$file"
+
+open "$file"
