afed7f67573c8cfba7ac27d79a07be9daed16551
Andy Ward <andy.ward.uk@gmail.com>
2024-12-19 07:51:22 +0000

    day 19 tidy

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -7,6 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Get input
     let (available, designs) = parse_input();
 
+    // Get number of valid arrangements for each design
     let composable = build_composable(&available, &designs);
 
     // Run parts
@@ -17,40 +18,55 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn part1(composable: &[usize]) -> u64 {
+    // Count the number of designs with at least one valid arrangement
     composable.iter().filter(|c| **c != 0).count() as u64
 }
 
 fn part2(composable: &[usize]) -> u64 {
+    // Sum the total number of arrangements
     composable.iter().sum::<usize>() as u64
 }
 
 fn build_composable(available: &FxHashSet<String>, designs: &[String]) -> Vec<usize> {
+    // Get max length of available
     let maxlen = available.iter().map(|a| a.len()).max().unwrap();
 
     designs
         .iter()
         .map(|design| {
+            // Get the design length
             let dlen = design.len();
+
+            // Create vector to hold number of composable for each position
             let mut composable = vec![0usize; dlen];
 
+            // Loop each design position in reverse
             for idx in (0..dlen).rev() {
-                let scan = maxlen.min(dlen - idx);
+                // Calculate max number of characters to scan
+                let max_scan = maxlen.min(dlen - idx);
 
-                for end in 1..=scan {
-                    let chunk = &design[idx..idx + end];
+                // Scan all substrings
+                for sublen in 1..=max_scan {
+                    // Get substring
+                    let substr = &design[idx..idx + sublen];
 
-                    if let Some(a) = available.get(chunk) {
-                        let next_idx = idx + a.len();
+                    // Substring in available?
+                    if let Some(available) = available.get(substr) {
+                        // Yes - calculate next position
+                        let next_idx = idx + available.len();
 
                         if next_idx == dlen {
+                            // Reached the end
                             composable[idx] += 1;
                         } else {
+                            // Add the number of composable at the next index
                             composable[idx] += composable[next_idx];
                         }
                     }
                 }
             }
 
+            // Return the number of composable at position 0
             composable[0]
         })
         .collect()
